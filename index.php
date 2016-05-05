ded47a7af6412b525a69a1a77529f5d447796e81
Chinmesh Manjrekar <Chinmesh Manjrekar>
2016-05-06 02:52:05 +0530

    My first Dynamic site

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		
+		<!-- Jquery UI CSS -->
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/thems/smoothness/jquery-ui.css" >
+		
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+
+		<!--jquery -->
+		<script src="//code.jquery.com/jquery-2.1.0.min.js﻿" ></script>
+		
+		<!--jquery UI -->
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui﻿.js" ></script>
+		
+		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	
+		<title>Hello</title>
+	
+	</head>
+	
+	<body>
+	
+		<div>
+			<h1>Beweare of the Bomb <i class="fa fa-cogs " aria-hidden="true"></i> </h1>
+		</div>
+		<div align="center">	
+			<i class="fa fa-bomb fa-5x " aria-hidden="true" ></i>
+		</div>
+	</body>
+	
+</html>
